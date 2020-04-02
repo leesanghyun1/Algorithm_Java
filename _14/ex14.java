4ef0f2d89c403de361e80d118b1dea6e1bc915e9
leesanghyun1 <dsw00573@naver.com>
2020-04-02 19:02:01 +0900

    별 출력

---
+++ _14/ex14.java
@@ -0,0 +1,25 @@
+package _14;
+//º°Ãâ·Â
+
+public class ex14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n = 6;
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(i < n - (j + 1)) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+				
+			}
+			System.out.println();
+		}
+		
+		System.out.print(13%10);
+	}
+
+}
